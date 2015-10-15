e8e2e58eeab18b914fc6e91d2d661b62ca8db409
Silas J. Matson <silas@infinite.red>
2015-10-14 20:15:18 -0700

    Debugging random crap.

--- lib/motion-fb.rb
+++ lib/motion-fb.rb
@@ -32,9 +32,7 @@ module Motion::Project
 
     def set_up_application_query_schemes
       self.info_plist["LSApplicationQueriesSchemes"] ||= []
-      [ "fbapi", "fbapi20130214", "fbapi20130410", "fbapi20130702", "fbapi20131010", "fbapi20131219", "fbapi20140410", "fbapi20140116", "fbapi20150313", "fbapi20150629", "fbauth", "fbauth2", "fb-messenger-api20140430" ].each do |scheme|
-        self.info_plist["LSApplicationQueriesSchemes"] << scheme
-      end
+      self.info_plist["LSApplicationQueriesSchemes"] = self.info_plist["LSApplicationQueriesSchemes"] | [ "fbapi", "fbapi20130214", "fbapi20130410", "fbapi20130702", "fbapi20131010", "fbapi20131219", "fbapi20140410", "fbapi20140116", "fbapi20150313", "fbapi20150629", "fbauth", "fbauth2", "fb-messenger-api20140430" ]
     end
 
     def set_up_cf_bundle_url_types(app_id)
